6ec06075ef60b484845b859faaa49973cadc64b0
Simone Balducci <93096843+SimonB00@users.noreply.github.com>
2021-12-04 12:34:39 +0100

    Add files via upload

--- VDP.cpp
+++ VDP.cpp
@@ -24,7 +24,7 @@ void VDP1(double x0, double y0, double dt, double mu, double T, std::string s) {
     outfile.close();
 }
 
-void VDP2(double x0, double y0, double dt, double mu, double T, std::string s) {        
+void VDP2(double x0, double y0, double dt, double mu, double T, std::string s) {        // Andamento temporale
     double t = 0;
     double steps = T/dt;
     double x = x0;
@@ -106,8 +106,37 @@ void VDPF1(double x0, double y0, double dt, double mu, double T, double w, doubl
     outfile.close();
 }
 
+double comparation(std::string s1, std::string s2, int N, int size1, int size2) {
+    std::ifstream is1{s1};      // Più rado
+    std::ifstream is2{s2};      // Più fitto
+    double t1 = 0.;
+    double t2 = 0.;
+    double x1 = 0.;
+    double x2 = 0.;
+    double diff = 0.;
+    std::vector<double> results;
+
+    for(int i = 0; i != size1; ++i) {
+        is1 >> t1;
+        for(int j = 0; t2 != t1; ++j) {
+            is2 >> t2;
+            if(t2 == t1) { 
+                is1 >> x1;
+                is2 >> x2;
+                diff = std::abs(x1-x2);
+                results.push_back(diff);
+            }
+            else {
+                is2 >> x2;
+            }
+        }
+    }
+    double max = *std::max_element(results.begin(),results.end());
+    return max;
+}
+
 int main() {
-    VDP1(1,1,0.01,1,20,"VDP1.dat");          // Traiettoria caratteristica dell'oscillatore
+    VDP1(1,1,0.1,1,20,"VDP1PP.dat");          // Traiettoria caratteristica dell'oscillatore
     /*VDP1(1,1,0.01,0,20,"VDP0.dat");          // Traiettorie per valori di mu: mu = 0
     VDP1(1,1,0.01,0.25,20,"VDP025.dat");       // mu = 0.25 
     VDP1(1,1,0.01,0.5,20,"VDP05.dat");        // mu = 0.5
@@ -124,7 +153,7 @@ int main() {
     VDP1(1,1,0.01,8,20,"VDP8.dat");          // mu = 8
     VDP2(1,1,0.01,2,35,"AndTemp.dat");       // mu = 2
     VDP4(1.5,1,0.01,1.5,15,"SingolaPar.dat");*/  // mu = 1.5
-    VDP1(1,1,0.01,1.5,20,"VDP15.dat");
+    VDP1(1,1,0.01,-0.5,20,"VDP-05.dat");
     VDPF1(1,1,0.01,8.5,200,0.6,1.2,"AndVDPF.dat");
 
     std::string name = "Anim";
@@ -136,10 +165,16 @@ int main() {
     std::mt19937 gen;
     std::uniform_real_distribution<> dis(-2.,2.);
 
+    VDP2(1,1,0.01,1,15,"Err1.dat");
+    VDP2(1,1,0.0001,1,15,"Err2.dat");
+    std::cout << "Max = " << comparation("Err1.dat","Err2.dat",100,1500,150000) << '\n';
+
+    /*
     for(int i = 0; i != N_; ++i) {
         std::string new_name = name + std::to_string(i) + ".dat";
         x = dis(gen);
         y = dis(gen);
         VDP1(x,y,0.01,mu,T,new_name);
     }
+    */
 }
